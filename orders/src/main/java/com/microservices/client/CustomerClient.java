1974dad746c97d190374053946c68ee97a3e5595
Mouaad Es-safryouy <mouaadessafryouy1@gmail.com>
2025-04-20 14:43:16 +0100

    add order ms eureka mysql to docker

--- orders/src/main/java/com/microservices/client/CustomerClient.java
+++ orders/src/main/java/com/microservices/client/CustomerClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer")
+@FeignClient(name = "customer", url = "http://host.docker.internal:8081")  // Use host.docker.internal
 public interface CustomerClient {
     @GetMapping("/api/v1/customers/{id}")
     CustomerDTO getCustomerById(@PathVariable Integer id);
--- orders/src/main/java/com/microservices/client/ProductClient.java
+++ orders/src/main/java/com/microservices/client/ProductClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product")
+@FeignClient(name = "product", url = "http://host.docker.internal:8082")  // Use host.docker.internal
 public interface ProductClient {
     @GetMapping("/api/v1/products/{id}")
     ProductDTO getProductById(@PathVariable Integer id);
